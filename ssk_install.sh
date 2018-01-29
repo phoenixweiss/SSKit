b365401d8d1b306039f7c93f7192e9f6c0b3a9b1
Paul Phönixweiß <phoenixweiss@users.noreply.github.com>
2018-01-29 13:42:17 +0300

    mailutils fix

--- ssk_install.sh
+++ ssk_install.sh
@@ -450,6 +450,9 @@ case $choice in
       debconf-set-selections <<< "postfix postfix/mailname string $SERVER_NAME"
       debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
       apt-get install -y postfix
+      
+      say "Install mailutils"
+      apt-get install -y mailutils
 
       # TODO
       # inet_protocols = ipv4
